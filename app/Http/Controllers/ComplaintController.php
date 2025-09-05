aea8593b598f3a19007029afb1bae9643eabdb7d
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-05 12:04:11 +0530

    refacr:added corect complaint controller

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Complaint;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ComplaintController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth'); // only logged-in users
+    }
+
+    // Show the form
+    public function create()
+    {
+        return view('complaints.create');
+    }
+
+    // Store complaint
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category' => 'required|string|max:100',
+            'complaint_text' => 'required|string',
+        ]);
+
+        Complaint::create([
+            'user_id' => Auth::id(),
+            'category' => $request->category,
+            'complaint_text' => $request->complaint_text,
+        ]);
+
+        return redirect()->back()->with('success', 'Your complaint has been submitted!');
+    }
 }
