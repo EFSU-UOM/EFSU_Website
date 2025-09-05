b356d59bb4ba3a040925cf330fcd88c4a5f7ef57
Suhas Dissanayake <suhasdissa@gmail.com>
2025-09-06 00:19:38 +0530

    fix: User factory not having access level field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\AccessLevel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('contact')->nullable();
-            $table->integer('access_level')->default(100);
+            $table->integer('access_level')->default(AccessLevel::USER->value);
             $table->rememberToken();
             $table->timestamps();
         });
