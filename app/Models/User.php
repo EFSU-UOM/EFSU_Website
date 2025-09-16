b4bdcc8999f2eba21922bac89fe3c97faf4cd673
Suhas Dissanayake <suhasdissa@gmail.com>
2025-09-16 19:27:39 +0530

    refactor: improve registration form

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'contact',
+        'batch',
         'password',
         'access_level',
     ];
